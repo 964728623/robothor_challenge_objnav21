743262fd8193dd02dd6939f06f42984a2027c299
MCXBB <964728623@qq.com>
2021-06-02 20:30:20 +0800

    allenact

---
+++ projects/objectnav_baselines/experiments/robothor/objectnav_robothor_base.py
@@ -0,0 +1,39 @@
+import os
+from abc import ABC
+
+import torch
+
+from projects.objectnav_baselines.experiments.objectnav_thor_base import (
+    ObjectNavThorBaseConfig,
+)
+
+
+class ObjectNavRoboThorBaseConfig(ObjectNavThorBaseConfig, ABC):
+    """The base config for all RoboTHOR ObjectNav experiments."""
+
+    THOR_COMMIT_ID = "bad5bc2b250615cb766ffb45d455c211329af17e"
+
+    DEFAULT_NUM_TRAIN_PROCESSES = 10 if torch.cuda.is_available() else 1  # change the process number : 12
+
+    TRAIN_DATASET_DIR = os.path.join(os.getcwd(), "datasets/robothor-objectnav/train")
+    VAL_DATASET_DIR = os.path.join(os.getcwd(), "datasets/robothor-objectnav/val")
+    TEST_DATASET_DIR = os.path.join(os.getcwd(), "datasets/robothor-objectnav/test")
+
+    TARGET_TYPES = tuple(
+        sorted(
+            [
+                "AlarmClock",
+                "Apple",
+                "BaseballBat",
+                "BasketBall",
+                "Bowl",
+                "GarbageCan",
+                "HousePlant",
+                "Laptop",
+                "Mug",
+                "SprayBottle",
+                "Television",
+                "Vase",
+            ]
+        )
+    )
